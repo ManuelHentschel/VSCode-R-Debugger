6b8a06e0600858477c76535982009c4d154f3df8
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2020-05-27 13:20:38 +0200

    Added test file for debug mode with main()

---
+++ test/R/testMain.R
@@ -0,0 +1,20 @@
+foo <- function(x, y) {
+  print(x)
+  print(y)
+  browser()
+  x + y
+}
+
+bar <- function(x, n) {
+  z <- x
+  for (i in seq_len(n)) {
+    print(i)
+    z <- foo(z, x)
+  }
+  z
+}
+
+main <- function(){
+  bar(2, 5)
+}
+
