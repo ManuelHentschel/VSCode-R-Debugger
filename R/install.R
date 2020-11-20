fff9e79238a4b2edbae60a1f80042a071816dbad
Kun Ren <renkun@outlook.com>
2020-11-20 17:29:52 +0800

    Revert to install.packages

--- R/install.R
+++ R/install.R
@@ -3,6 +3,7 @@ if (is.null(repos) || identical(repos, c(CRAN = "@CRAN@"))) {
   options(repos = c(CRAN = "https://cloud.r-project.org/"))
 }
 
+install.packages(c("jsonlite", "R6"))
+
 url <- commandArgs(trailingOnly = TRUE)[[1]]
-install.packages("remotes")
-remotes::install_url(url, dependencies = TRUE)
+install.packages(url, repos = NULL)
